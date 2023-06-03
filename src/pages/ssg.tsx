b5f2cd3c0c8a0f13a067266f65c6f6dc07fa1f41
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-03 09:27:58 +0900

    edit src/pages/ssg.tsx

--- src/pages/ssg.tsx
+++ src/pages/ssg.tsx
@@ -1,9 +1,16 @@
-import { NextPage } from "next"
+import { GetStaticProps, NextPage, NextPageContext } from "next"
 import Head from 'next/head'
 
-type SSGProps = {}
+//ページコンポーネントのpropsの型定義
+type SSGProps = {
+  message: string
+}
 
-const SSG: NextPage<SSGProps> = () => {
+//SSGはGetStaticPropsが返したpropsを受け取ることができる
+//NextPage<SSGProps>はmessage: stringのみを受け取って生成されるページの型
+const SSG: NextPage<SSGProps> = (props) => {
+  const { message } = props
+
   return (
     <div>
       <Head>
@@ -14,9 +21,24 @@ const SSG: NextPage<SSGProps> = () => {
         <p>
           このページは静的サイト生成によってビルド時に生成されたページです
         </p>
+        <p>{ message }</p>
       </main>
     </div>
   )
+}
+
+//getStaticPropsはビルド時に実行される
+//GetStaticProps<SSGProps>はSSGPropsを引数にとるgetStaticPropsの型
+export const getStaticProps: GetStaticProps<SSGProps> = async(context) => {
+  const timestamp = new Date().toDateString()
+  const message = `${timestamp} にgetStaticPropsが実行されました`
+  console.log(message)
+  return {
+    //ここで返したpropsを元にページコンポーネントを描画する
+    props: {
+      message,
+    },
+  }
 }
 
 export default SSG
