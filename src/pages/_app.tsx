6c6345855eb48a6e83221e1f5c1896aede389d5f
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-04 20:35:07 +0900

    modified:   src/pages/_app.tsx new file:   src/pages/theme-sample.tsx new file:   theme.ts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../../theme'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  // styled-componentsでテーマを使用するためにThemeProviderを置く
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
+
+export default MyApp
